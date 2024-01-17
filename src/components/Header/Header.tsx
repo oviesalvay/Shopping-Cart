9b7d669dbc1bb79bb53b331c4fbe14271276f48e
oviesalvay <oviesalvay@gmail.com>
2024-01-17 12:03:18 +0100

    worked on header

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,35 @@
+import logo from './logo.png'
+
+
+const Header = () =>{
+    return(
+        <>
+        <div>
+        <img src={logo}alt=""/>
+        </div>
+        <div>
+        <p>Laptops</p>
+        </div>
+        <div>
+     <p>Desktop PCs</p>
+        </div>
+        <div>
+        <p>Networking Devices</p>
+        </div>
+        <div>
+        <p>Printers & Scanners</p>
+        </div>
+        <div>
+        <p>PC Parts</p>
+        </div>
+        <div>
+            <p>All Other Products</p>
+        </div>
+        <div>
+            <p>Repairs</p>
+        </div>
+        
+        </>
+    )
+}
+export default Header;
