76df43851899348f0b5f188187c85072ad2a8c3d
oviesalvay <oviesalvay@gmail.com>
2024-01-19 22:50:44 +0100

    fix

--- src/pages/NewProducts/NewProduct.tsx
+++ src/pages/NewProducts/NewProduct.tsx
@@ -131,7 +131,7 @@ const NewProducts =()=>{
                                                
                                     </div>
                                     </SwiperSlide>
-                                    {/* </div> */}
+                                    
                                 </Swiper>
         </section>
     )
